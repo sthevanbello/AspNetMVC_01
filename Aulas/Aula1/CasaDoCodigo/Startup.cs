8ad3b6dd92cfda26b39f26c26eb4b0d972faf671
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-05-04 23:14:33 -0300

    First Migration

--- Aulas/Aula1/CasaDoCodigo/Startup.cs
+++ Aulas/Aula1/CasaDoCodigo/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -22,6 +23,11 @@ namespace CasaDoCodigo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            string connectionString = Configuration.GetConnectionString("Default");
+
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString)
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,7 +43,7 @@ namespace CasaDoCodigo
                 app.UseExceptionHandler("/Home/Error");
             }
 
-            
+
             app.UseStaticFiles();
 
             app.UseMvc(routes =>
